6b255d96524cdba35101f9e5de7107f6b0495e7f
mihir2452005 <mihir2452005@gmail.com>
2026-05-20 01:15:04 +0530

    chore: solve explicit execution environment root for pyright and remove typed default for password init parameter

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,6 +4,6 @@ class User(db.Model):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
 
-    def __init__(self, email: str, password: any = None):
+    def __init__(self, email: str, password=None):
         self.email = email
         self.password = password
