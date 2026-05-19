d0eed92664bb776686e41239185a1c8ee809a496
mihir2452005 <mihir2452005@gmail.com>
2026-05-19 17:57:55 +0530

    chore: add __init__ to User model to resolve type checker warnings

--- backend/models/user.py
+++ backend/models/user.py
@@ -5,3 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
+
+    def __init__(self, email: str, password: str):
+        self.email = email
+        self.password = password
